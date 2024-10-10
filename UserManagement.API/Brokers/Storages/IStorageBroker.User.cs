ab2e601cd5a5161416b4d70a998d7e656af589bb
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-10-10 18:32:45 +0500

    BROKERS: CRUD User

--- UserManagement.API/Brokers/Storages/IStorageBroker.User.cs
+++ UserManagement.API/Brokers/Storages/IStorageBroker.User.cs
@@ -2,9 +2,19 @@
 // Copyright (c) Coalition Of Good-Hearted Engineers
 // Free To Use To Find Comfort And Peace
 //--------------------------------------------------
+using System.Linq;
+using System.Threading.Tasks;
+using UserManagement.API.Models.Foundations.Users;
+
 namespace UserManagement.API.Brokers.Storages
 {
     public partial interface IStorageBroker
     {
+        ValueTask<User> InsertUserAsync(User user);
+        ValueTask<IQueryable<User>> SelectAllUsersAsync();
+        ValueTask<User> SelectUserByIdAsync(int userId);
+        ValueTask<User> UpdateUserAsync(User user);
+        ValueTask<User> DeleteUserAsync(User user);
+        ValueTask<User> SelectUserByEmailAsync(string email);
     }
 }
